ebba45ec2ec5511b94c6da37415caf61b958b007
Elena Zuin <elena.zuin3@gmail.com>
2024-08-06 10:10:04 +0200

    Add basilisk environment

---
+++ R/basilisk.R
@@ -0,0 +1,5 @@
+#' @importFrom basilisk BasiliskEnvironment
+py_env <- BasiliskEnvironment(envname = "py_env", pkgname = "scIntegration",
+                              packages = c("python==3.10.12", "scanorama==1.7.4", "scanpy==1.10.1",
+                                           "python-igraph==0.11.4", "leidenalg==0.10.2", "scikit-learn==1.4.2",
+                                           "anndata==0.10.7", "bbknn==1.6.0", "scvi-tools==1.1.2", "lightning==2.0.9.post0"))
