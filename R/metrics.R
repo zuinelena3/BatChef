47a28eb8202f7399afba28cedfb2336d185e48c6
Elena Zuin <elena.zuin3@gmail.com>
2024-08-07 17:00:24 +0200

    Add metrics function to evaluate integration performance

---
+++ R/metrics.R
@@ -0,0 +1,34 @@
+#' Common quantitative metrics
+#'
+#' @param anndata anndata object, computed by clustering function
+#' @param cell_type string specifying cell-type labels
+#' @param batch string specifying batch
+#' @param reduction string specifying the integration reduction
+#'
+#' @importFrom basilisk basiliskStart basiliskStop basiliskRun
+#' @importFrom reticulate import
+#' @importFrom lisi compute_lisi
+#' @importFrom stats median
+#' @export
+#'
+metrics <- function(anndata, cell_type, batch, reduction) {
+  proc <- basiliskStart(py_env)
+  on.exit(basiliskStop(proc))
+
+  out <- basiliskRun(proc = proc, fun = function(anndata, cell_type, batch, reduction) {
+    scanpy <- import("scanpy")
+    sklearn <- import("sklearn")
+
+    nmi <- sklearn$metrics$normalized_mutual_info_score(labels_true = anndata$obs[, cell_type], labels_pred = anndata$obs$cluster)
+    ari <- sklearn$metrics$adjusted_rand_score(labels_true = anndata$obs[, cell_type], labels_pred = anndata$obs$cluster)
+    casw <- sklearn$metrics$silhouette_score(anndata$obsm[paste0("X_", reduction)], anndata$obs[, cell_type])
+    clisi <- median(compute_lisi(anndata$obsm[paste0("X_", reduction)], data.frame(ct = anndata$obs[, cell_type]), "ct")$ct)
+    homogeneity <- sklearn$metrics$homogeneity_score(anndata$obs[, cell_type], anndata$obs$cluster)
+
+    iasw <- sklearn$metrics$silhouette_score(anndata$obsm[paste0("X_", reduction)], anndata$obs[, batch])
+    ilisi <- median(compute_lisi(anndata$obsm[paste0("X_", reduction)], data.frame(batch = anndata$obs[, batch]), "batch")$batch)
+
+    df <- data.frame(nmi, ari, casw, clisi, homogeneity, iasw, ilisi)
+    return(df)
+  }, anndata = anndata, cell_type = cell_type, batch = batch, reduction = reduction)
+}
---
+++ tests/testthat/test-metrics.R
@@ -0,0 +1,7 @@
+test_that("metrics function works", {
+  data <- simulated_data(nGenes = 1000, batchCells = c(155, 150), group.prob = c(0.3, 0.5, 0.2))
+
+  out_pre <- clustering(data, cell_type = "Group", reduction = "pca")
+  metrics_pre <- metrics(anndata = out_pre, cell_type = "Group", batch = "Batch", reduction = "pca")
+  expect_type(metrics_pre, "list")
+})
