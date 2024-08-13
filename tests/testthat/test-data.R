8949253375347996d4d5f59a10fd46ab39a4f19e
Elena Zuin <elena.zuin3@gmail.com>
2024-08-13 14:09:33 +0200

    Edit simulated_data function

--- tests/testthat/test-data.R
+++
@@ -1,4 +0,0 @@
-test_that("fake_data function works", {
-  data <- simulated_data(nGenes = 1000, batchCells = c(155, 150), group.prob = c(0.3, 0.5, 0.2))
-  expect_s4_class(data, "SingleCellExperiment")
-})
---
+++ tests/testthat/test-simulated_data.R
@@ -0,0 +1,4 @@
+test_that("fake_data function works", {
+  data <- simulated_data(nGenes = 1000, batchCells = c(155, 150), group.prob = c(0.3, 0.5, 0.2), ncomp = 10)
+  expect_s4_class(data, "SingleCellExperiment")
+})
