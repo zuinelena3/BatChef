7fa10cfe8cf5a29ffa0b4b0acf78f69a590db737
zuinelena3 <elena.zuin3@gmail.com>
2026-02-26 11:14:53 +0100

    Edit coding style in testing

--- tests/testthat/test-suggested_method.R
+++ tests/testthat/test-suggested_method.R
@@ -1,10 +1,12 @@
 test_that("Suggested method", {
   suppressMessages({
-    sim <- simulate_data(n_genes = 1000, batch_cells = c(130, 110),
-                         group_prob = c(0.3, 0.2, 0.5), n_hvgs = 1000,
-                         compute_pca = FALSE, output_format = "Seurat")
+    sim <- simulate_data(
+      n_genes = 1000, batch_cells = c(130, 110),
+      group_prob = c(0.3, 0.2, 0.5), n_hvgs = 1000,
+      compute_pca = FALSE, output_format = "Seurat"
+    )
 
     pred <- suggested_method(input = sim, batch = "Batch")
-    expect_true(inherits(pred, "character"))
+    expect_true(inherits(pred, "ggplot"))
   })
 })
